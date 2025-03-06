a9d599876db2b06292913a0e2cd517b33f33af0d
aidarjeenbekov <aidarjeenbekov444@gmail.com>
2025-03-06 10:07:41 +0600

    Initial commit

---
+++ Afisha/movie_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('directors/', views.DirectorList.as_view(), name='director-list'),
+    path('directors/<int:pk>/', views.DirectorDetail.as_view(), name='director-detail'),
+    path('movies/', views.MovieList.as_view(), name='movie-list'),
+    path('movies/<int:pk>/', views.MovieDetail.as_view(), name='movie-detail'),
+    path('reviews/', views.ReviewList.as_view(), name='review-list'),
+    path('reviews/<int:pk>/', views.ReviewDetail.as_view(), name='review-detail'),
+]
